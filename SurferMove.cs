b29e9921962b02f9f1ffd2f6cbdcee6d856d989e
mptayag27 <67673670+mptayag27@users.noreply.github.com>
2022-06-06 18:49:10 -0700

    Add files via upload

---
+++ SurferMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurferMove : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody2D rb;
+    GameObject surf;
+    public float maxSpeed = 6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        surf = this.gameObject;
+        rb = surf.GetComponent<Rigidbody2D>();
+        //Vector3 maxSpeed = (0f, 10f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.velocity = new Vector2(0f, 0f);
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            //rb.velocity = new Vector2(-4f, -10f);
+            rb.AddForce(transform.up * -19);
+            anim.SetInteger("move", 1);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            //rb.velocity = new Vector2(4f, 10f);
+            rb.AddForce(transform.up * 19);
+            anim.SetInteger("move", 2);
+        }
+        else if (!(Input.GetKey(KeyCode.RightArrow)) && !(Input.GetKey(KeyCode.D)) && !(Input.GetKey(KeyCode.LeftArrow)) && !(Input.GetKey(KeyCode.A)))
+        {
+            anim.SetInteger("move", 0);
+        }
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+    }
+}
---
+++ shellspawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shellspawn : MonoBehaviour
+{
+    float time = 0.0f;
+    float interval = 2.0f;
+    GameObject rat;
+    float vx;
+    float vy;
+    float vy2;
+    float vy3;
+    public GameObject prefab = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rat = GameObject.Find("ball (1)");
+        vx = gameObject.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > interval)
+        {
+            float k = Random.Range(-4, 4);
+            //print ("k");
+                vy = rat.transform.position.y + k;
+                shoot();
+                time = 0f;
+        }
+    }
+    void shoot()
+    {
+        GameObject obj = (GameObject)Instantiate(prefab, new Vector2(vx, vy), Quaternion.identity);
+        obj.GetComponent<Rigidbody2D>().velocity = new Vector2(-17f, 0f);
+    }
+}
